0b2088ee5808a9ac84a7d6cace99d9f14d6cb6d0
Gergő Csontos <gergo.csontos.123@gmail.com>
2021-09-08 22:04:37 +0200

    add responsive printBoard (flexible field-size + padding)

--- src/com/battleship/util/Display.java
+++ src/com/battleship/util/Display.java
@@ -4,10 +4,13 @@ package com.battleship.util;
 import com.battleship.Board;
 import com.battleship.Square;
 
+import java.util.Arrays;
+
 public class Display {
     private static Display single_instance = null;
 
-    private Display() {}
+    private Display() {
+    }
 
     public static Display getInstance() {
         if (single_instance == null)
@@ -16,26 +19,26 @@ public class Display {
         return single_instance;
     }
 
-    public void printMessage(String message){
+    public void printMessage(String message) {
         System.out.print(message);
     }
 
-    public void printMessageLine(String message){
+    public void printMessageLine(String message) {
         System.out.println(message);
     }
 
-    public void printMenuOptions(String[] menuOptions, String message){
-        for(String menuOption: menuOptions){
+    public void printMenuOptions(String[] menuOptions, String message) {
+        for (String menuOption : menuOptions) {
             printMessageLine(menuOption);
         }
         printMessage(message);
     }
 
-    public void clear(){
-        try{
+    public void clear() {
+        try {
             String operatingSystem = System.getProperty("os.name"); //Check the current operating system
 
-            if(operatingSystem.contains("Windows")){
+            if (operatingSystem.contains("Windows")) {
                 ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
                 Process startProcess = pb.inheritIO().start();
                 startProcess.waitFor();
@@ -45,39 +48,49 @@ public class Display {
 
                 startProcess.waitFor();
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public void printBoard(Board board, boolean hiddenIcon){
+    public void printBoard(Board board, boolean hiddenIcon) {
+
+        int fieldSpace = 3;
+
+        String padding = "   ";
+
 
         System.out.println();
         Square[][] ocean = board.getOcean();
-        String headerLine = "  ";
+        System.out.print(padding);
         for (int i = 1; i <= ocean.length; i++) {
-            headerLine += i % 10 + " ";//💥("collision")🌊("wave")☁("cloud")  🟦("blue square"), emoji icon possibilities
+            System.out.printf("%" + fieldSpace + "s", i );  // do we want this...
+            //💥("collision")🌊("wave")☁("cloud")  🟦("blue square"), emoji icon possibilities
         }
-        System.out.println(headerLine);
+        System.out.println();
 
         for (int i = 0; i < board.getSize(); i++) {
-            System.out.print(Character.toChars(65 + i));
-            System.out.print(" ");
+            String letter = String.valueOf(Character.toChars(65 + i));
+            System.out.printf("%" + fieldSpace + "s", letter);
+
 
             if (hiddenIcon) {
                 for (int j = 0; j < board.getSize(); j++) {
-                    System.out.print(ocean[i][j].getHidden() + " ");
+                    System.out.printf("%" + fieldSpace + "s", ocean[i][j].getHidden());
                 }
             } else {
                 for (int j = 0; j < board.getSize(); j++) {
-                    System.out.print(ocean[i][j].getShown() + " ");
+                    System.out.printf("%" + fieldSpace + "s", ocean[i][j].getShown());
                 }
             }
 
-            System.out.print(Character.toChars(65 + i));
-            System.out.print(" ");
+            System.out.printf("%" + (fieldSpace - 1) + "s", letter);
             System.out.println();
         }
-        System.out.println(headerLine);
+        System.out.print(padding);
+        for (int i = 1; i <= ocean.length; i++) {
+            System.out.printf("%" + fieldSpace + "s", i % 10);  // ... or this ? :D
+        }
+        System.out.println();
     }
 }
